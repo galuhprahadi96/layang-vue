528d7157a8ef1c123f7eab280884f1cc531fc4d0
galuhprahadi96 <galuhprahadi96@gmail.com>
2020-10-19 14:28:08 +0700

    fix in add

--- src/store/modules/friend.js
+++ src/store/modules/friend.js
@@ -5,30 +5,29 @@ export default {
   mutations: {},
   actions: {
     searchFriend(context, payload) {
-        return new Promise((resolve, reject) => {
-          axios
-            .get(`${process.env.VUE_APP_URL}/users/search?keyword=${payload}`)
-            .then(res => {
-              resolve(res.data)
-            })
-            .catch(err => {
-              reject(err.response)
-            })
-        })
+      return new Promise((resolve, reject) => {
+        axios
+          .get(`${process.env.VUE_APP_URL}/users/search?keyword=${payload}`)
+          .then(res => {
+            resolve(res.data)
+          })
+          .catch(err => {
+            reject(err.response)
+          })
+      })
     },
     addFriend(context, payload) {
-        return new Promise((resolve, reject) => {
-          axios
-            .post(`${process.env.VUE_APP_URL}/friend`, payload)
-            .then(res => {
-              resolve(res.data)
-            })
-            .catch(err => {
-              reject(err.response)
-            })
-        })
-      }
-    },
-    
-    getters: {}
-  }
+      return new Promise((resolve, reject) => {
+        axios
+          .post(`${process.env.VUE_APP_URL}/friend`, payload)
+          .then(res => {
+            resolve(res.data)
+          })
+          .catch(err => {
+            reject(err.response)
+          })
+      })
+    }
+  },
+  getters: {}
+}
